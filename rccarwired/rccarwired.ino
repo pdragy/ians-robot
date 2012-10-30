0cbe78d96ba2859295e95b2b8b864432b1e7dc51
Paul Draghicescu <pdragy@gmail.com>
2012-10-30 13:48:35 -0700

    added comments, formatting

--- rccarwired/rccarwired.ino
+++ rccarwired/rccarwired.ino
@@ -1,3 +1,9 @@
+// Arduino sketch for remote control car. 
+// Can go backwards and forwards turn right and left
+//
+// last modified 10/30
+// 
+
 #define motoron 2
 #define forward 5
 #define reverse 3
@@ -7,14 +13,12 @@
 #define steerin 4
 #define speedin 7
 
-int d=0;
-int Y=0;
-int fwd= 0;
-int rev= 0;
-int rght= 0;
-int lft= 0;
-
-int main() {}
+int d = 0;
+int Y = 0;
+int fwd = 0;
+int rev = 0;
+int rght = 0;
+int lft = 0;
 
 void setup() {                
     pinMode(steerin, INPUT);         // set receiver pins
@@ -25,53 +29,45 @@ void setup() {
 }
 
 void loop() {
-  d=pulseIn(speedin, HIGH);       //receiver drive output
-  //Serial.print(d);
-  //Serial.println();
-  fwd= map(d, 1600, 950, 0, 255);
-  rev= map(d, 1600, 2100, 0, 255);
-  
-  if(d <1525 && d> 100){                  //go forward
-    analogWrite(forward, fwd);
-    analogWrite(reverse, 0);
-  }
-  else if(d >= 1525 && d<= 1675){        //if at nuetral keep motors off
-    
-   analogWrite(forward, 0);
-   analogWrite(reverse, 0);
-  }
-  else if (d >1675 && d < 2100){          //go reverse
-    analogWrite(reverse, rev);
-    analogWrite(forward, 0);
-  }
-  else if (d<=100 && d >= 2100){          //shut motors off if odd data or remote is off
-     analogWrite(forward, 0);
-   analogWrite(reverse, 0);
-  }
-  //Serial.print(d);
-  //Serial.println();
-  //steering
-    Y=pulseIn(steerin, HIGH);            // receiver data
-  //Serial.print(Y);
-  //Serial.println();
-  rght= map(Y, 1500, 2050, 0, 255);
-  lft= map(Y, 1500, 1000, 0, 255);
-  if(Y <1425 && Y> 100){                // go left
-    analogWrite(left, lft);
-    analogWrite(right, 0);
-  }
-  else if(Y >= 1425 && Y<= 1600){        // at neutral position shut motor off
-   analogWrite(right, 0);
-   analogWrite(left, 0);
-  }
-  else if (Y >1600 && Y < 2100){          // turn right
-    analogWrite(right, rght);
-    analogWrite(left, 0);
-  }
-  else if (Y<=100 &&  Y >= 2100){        // shut motors off when remote is off
-     analogWrite(right, 0);
-   analogWrite(left, 0);
-  }
-  Serial.print(lft);
-  Serial.println();
+    d = pulseIn(speedin, HIGH);       //receiver drive output
+    //Serial.print(d);
+    //Serial.println();
+    fwd= map(d, 1600, 950, 0, 255);
+    rev= map(d, 1600, 2100, 0, 255);
+    if (d < 1525 && d > 100){                  //go forward
+        analogWrite(forward, fwd);
+        analogWrite(reverse, 0);
+    } else if(d >= 1525 && d <= 1675) {        //if at nuetral keep motors off
+        analogWrite(forward, 0);
+        analogWrite(reverse, 0);
+    } else if (d > 1675 && d < 2100) {          //go reverse
+        analogWrite(reverse, rev);
+        analogWrite(forward, 0);
+    } else if (d <= 100 && d >= 2100) {          //shut motors off if odd data or remote is off
+        analogWrite(forward, 0);
+        analogWrite(reverse, 0);
+    }
+    //Serial.print(d);
+    //Serial.println();
+    //steering
+    Y = pulseIn(steerin, HIGH);            // receiver data
+    //Serial.print(Y);
+    //Serial.println();
+    rght = map(Y, 1500, 2050, 0, 255);
+    lft = map(Y, 1500, 1000, 0, 255);
+    if(Y < 1425 && Y > 100){                // go left
+        analogWrite(left, lft);
+        analogWrite(right, 0);
+    } else if (Y >= 1425 && Y <= 1600) {        // at neutral position shut motor off
+        analogWrite(right, 0);
+        analogWrite(left, 0);
+    } else if (Y > 1600 && Y < 2100) {          // turn right
+        analogWrite(right, rght);
+        analogWrite(left, 0);
+    } else if (Y <= 100 &&  Y >= 2100) {        // shut motors off when remote is off
+        analogWrite(right, 0);
+        analogWrite(left, 0);
+    }
+    Serial.print(lft);
+    Serial.println();
 }
