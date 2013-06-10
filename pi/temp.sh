0d574deb3b5202d76501d0047ef1be09e7946cef
Paul Draghicescu <pdragy@gmail.com>
2013-06-10 02:42:46 -0700

    added script which prints gpu and cpu temperature

---
+++ pi/temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0/1000))
+cpuTemp2=$(($cpuTemp0/100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+
+gpuTemp0=$(/opt/vc/bin/vcgencmd measure_temp)
+
+echo CPU temp=$cpuTemp1"."$cpuTempM"'C"
+echo GPU $gpuTemp0
